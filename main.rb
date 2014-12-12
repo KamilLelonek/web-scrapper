61f634f1b741f7a9ec3414f1092deee4360a97e1
Kamil Lelonek <squixy.sln@gmail.com>
2014-12-12 21:34:33 +0100

    Dumping use case orchestration

---
+++ main.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+require_relative 'dump_queue'
+
+DumpQueue.new.()
+
+__END__
+
+Expected message:
+
+{
+  items: [
+    {
+      name: '',
+      url:  '',
+      logo: ''
+    },
+    ...
+  ]
+}
